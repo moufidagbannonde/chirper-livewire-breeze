71b1f1e3f8edc7fe5d6d18cead4b2fe954484721
Moufid Agbannonde <magbannonde@gmail.com>
2024-07-31 20:08:40 +0200

    dashboard, register, login with livewire

---
+++ resources/views/Chirps.blade.php
@@ -0,0 +1,8 @@
+<div>
+    Waste no more time arguing what a good man should be, be one. - Marcus Aurelius
+</div>
+<x-app-layout>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <livewire:chirps.create/>
+    </div>
+</x-app-layout>
